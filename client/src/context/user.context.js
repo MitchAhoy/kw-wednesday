e5fcff2c9225b002bc24421962134b91c6aaaac7
MitchAhoy <51808358+MitchAhoy@users.noreply.github.com>
2020-11-25 23:28:28 +1100

    fetch user from database

--- client/src/context/user.context.js
+++ client/src/context/user.context.js
@@ -1,2 +1,29 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import axios from 'axios'
 
+export const UserContext = createContext()
+
+export const UserContextProvider = ({ children }) => {
+
+    const [user, setUser] = useState({})
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const res = await axios.get('/api/current_user')
+                const data = res.data
+                if (data) setUser(data)
+                return
+            } catch (err) {
+                throw new Error(err)
+            }
+        }
+        fetchUser()
+    }, [])
+
+    return (
+        <UserContext.Provider value={}>
+            {...children}
+        </UserContext.Provider>
+    )
+} 
